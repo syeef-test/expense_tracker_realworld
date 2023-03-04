f1aab3b455222fa7c1bd8188b9b029e0bf3c7e37
syeef-test <syeefislam@yahoo.com>
2023-03-04 07:38:35 +0530

    bycryptjs added as bycrypt not working and encrypted password for signin and login

--- expense_tracker_backend/controllers/userController.js
+++ expense_tracker_backend/controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require("../models/userModel");
-
+const bcrypt = require('bcryptjs');
 exports.postSignup = async (req, res, next) => {
   try {
     //console.log(req.body);
@@ -18,19 +18,25 @@ exports.postSignup = async (req, res, next) => {
     if (typeof user !== "undefined" && user.length > 0) {
       res.status(401).json({ error: "User Allready Exist with same email" });
     } else {
-      const insertData = await User.create({
-        name: name,
-        email: email,
-        password: password,
-      });
 
-      if (insertData) {
-        data = insertData.toJSON();
-        console.log(data);
-        res.status(201).json({ message: "Sign Up Succesful", data: data });
-      } else {
-        res.status(401).json({ error: "Sign Up unsuccesful" });
-      }
+      const saltrounds = 10;
+      bcrypt.hash(password, saltrounds, async (err, hash) => {
+        insertData = await User.create({ name, email, password: hash });
+        res.status(201).json({ message: "Sign Up Succesful", data: insertData.toJSON() });
+      });
+      // const insertData = await User.create({
+      //   name: name,
+      //   email: email,
+      //   password: password,
+      // });
+
+      // if (insertData) {
+      //   //data = insertData.toJSON();
+      //   //console.log(data);
+      //   res.status(201).json({ message: "Sign Up Succesful", data: data });
+      // } else {
+      //   res.status(401).json({ error: "Sign Up unsuccesful" });
+      // }
     }
   } catch (error) {
     console.log(error.toJSON());
@@ -45,22 +51,35 @@ exports.postLogin = async (req, res, next) => {
 
 
     const user = await User.findAll({
-      attributes: ["email"],
+      attributes: ["email", "password"],
       where: { email: req.body.email },
     });
 
     if (typeof user !== "undefined" && user.length > 0) {
 
-      const checkEmailPwd = await User.findAll({
-        attributes: ['email', 'password'],
-        where: { email: email, password: password }
-      });
-
-      if (typeof checkEmailPwd !== "undefined" && checkEmailPwd.length > 0) {
+      //console.log(user[0].password);
+      //used bycryptjs insted of bycrypt as bycrypt is not instaling
+      const match = await bcrypt.compareSync(req.body.password,user[0].password);
+      //console.log(match);
+      if (match) {
         res.status(200).json({ message: "User Login Succesful" });
       } else {
         res.status(401).json({ error: "User Not authorized" });
       }
+
+      // const checkEmailPwd = await User.findAll({
+      //   attributes: ['email', 'password'],
+      //   where: { email: email, password: password }
+      // });
+
+      // if (typeof checkEmailPwd !== "undefined" && checkEmailPwd.length > 0) {
+      //   res.status(200).json({ message: "User Login Succesful" });
+      // } else {
+      //   res.status(401).json({ error: "User Not authorized" });
+      // }
+
+
+
     }
     else {
       res.status(404).json({ error: "User Does not exist" });
