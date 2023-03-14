259502f28266970260e8339812f5ecc6313ffab4
syeef-test <syeefislam@yahoo.com>
2023-03-14 20:42:58 +0530

    transaction implemented in addexpense and delete expense

--- expense_tracker_backend/controllers/expenseController.js
+++ expense_tracker_backend/controllers/expenseController.js
@@ -1,39 +1,52 @@
 const Expense = require("../models/expenseModel");
 const User = require("../models/userModel");
+const sequelize = require("../util/database");
+
+
+
 
 exports.addExpense = async (req, res, next) => {
-  try {
-    const expenseamount = req.body.expsenseAmount;
-    const description = req.body.description;
-    const category = req.body.category;
+    const t = await sequelize.transaction();    //unmanged transaction
+    try {
+        const expenseamount = req.body.expsenseAmount;
+        const description = req.body.description;
+        const category = req.body.category;
 
-    const insertData = await Expense.create({
-        expenseamount:expenseamount,
-        description:description,
-        category:category,
-        userId:req.user.id
-    });
-    console.log(insertData);
 
-    const userDetails = await User.findOne({where:{id:req.user.id}});
-    const oldAmount = userDetails.totalExpense;
-    const newAmount = parseFloat(oldAmount)+parseFloat(expenseamount);
-    //console.log(newAmount);
-    const updateUser = await User.update({totalExpense:newAmount},{
-        where:{
-            id:req.user.id
-        }
-    });
 
-    if(insertData){
+        const insertData = await Expense.create({
+            expenseamount: expenseamount,
+            description: description,
+            category: category,
+            userId: req.user.id
+        }, { transaction: t });
+        //console.log(insertData);
+
+
+        const oldAmount = req.user.totalExpense;
+        const newAmount = parseFloat(oldAmount) + parseFloat(expenseamount);
+        //console.log(newAmount);
+        const updateUser = await User.update({ totalExpense: newAmount }, {
+            where: {
+                id: req.user.id,
+            },
+            transaction: t
+        });
+
+        await t.commit();
+        //throw new Error();
         res.status(201).json({ message: "Eepense Added", data: insertData.toJSON() });
-    }else{
-        res.status(401).json({ error: "Eepense Not Added"});
-    }
 
-  } catch (error) {
-    console.log(error);
-  }
+
+
+    } catch (error) {
+        if (t) {
+            await t.rollback();
+            res.status(401).json({ error: "Eepense Not Added" });
+        }
+
+        console.log(error);
+    }
 };
 
 // exports.deleteExpense = async(req,res,next)=>{
@@ -46,51 +59,58 @@ exports.addExpense = async (req, res, next) => {
 // }
 
 exports.getExpense = async (req, res, next) => {
-    try{
-        
-       const expenseData = await Expense.findAll({where:{userId:req.user.id}});
-       if(expenseData){
-        //console.log(req.user.ispremiumuser);
-        res.status(200).json({ message: "Data Found", data: expenseData});
-       }
+    try {
+
+        const expenseData = await Expense.findAll({ where: { userId: req.user.id } });
+        if (expenseData) {
+            //console.log(req.user.ispremiumuser);
+            res.status(200).json({ message: "Data Found", data: expenseData });
+        }
     }
-    catch(error){
+    catch (error) {
         console.log(error);
     }
 }
 
-exports.deleteExpense = async(req, res, next) => {
-    try{
+exports.deleteExpense = async (req, res, next) => {
+    const t = await sequelize.transaction();    //unmanged transaction
+    try {
         const expenseId = req.params.id;
         //console.log(expenseId);
-        
 
-        const expenseDetails  = await Expense.findOne({where:{id:expenseId}});
+
+        const expenseDetails = await Expense.findOne({ where: { id: expenseId } ,transaction:t});
         const expenseamount = parseFloat(expenseDetails.expenseamount);
 
-        const userDetails = await User.findOne({where:{id:req.user.id}});
-        const oldAmount = userDetails.totalExpense;
-        const newAmount = parseFloat(oldAmount)-parseFloat(expenseamount);
+
+        const oldAmount = req.user.totalExpense;
+        const newAmount = parseFloat(oldAmount) - parseFloat(expenseamount);
         //console.log(newAmount);
-        const updateUser = await User.update({totalExpense:newAmount},{
-            where:{
-                id:req.user.id
-            }
+        const updateUser = await User.update({ totalExpense: newAmount }, {
+            where: {
+                id: req.user.id
+            },
+            transaction:t
         });
 
-        const deleteData = await Expense.destroy({where:{id:expenseId,userId:req.user.id}});
+        const deleteData = await Expense.destroy({ where: { id: expenseId, userId: req.user.id },transaction:t });
+
+        await t.commit();
+        //throw new Error();
+        res.status(200).json({ message: "Deleted successfully" });
+       
+            
         
-        if(deleteData){
-            res.status(200).json({ message: "Deleted successfully" });
-        }else{
+    }
+    catch (error) {
+        if(t){
+            await t.rollback();
             res.status(404).json({ message: "record not found" });
         }
-    }
-    catch(error){
         console.log(error);
     }
-     
-  };
+
+};
 
 
 //   exports.isPremiuemUser = (req,res,next)=>{
